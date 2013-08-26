543d7b1127bc9b7f6f909a0bd905c681f1f3be04
youchan <youchan01@gmail.com>
2013-08-26 20:13:03 +0900

    redcarpet

--- diary.rb
+++ diary.rb
@@ -1,10 +1,15 @@
 # coding: utf-8
 
-require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/reloader'
 require 'redcarpet'
 
-get "/" do
-  @content = "ここに記事がはいります。"
-  erb :index
+class Diary < Sinatra::Base
+  set :sessions, true
+
+  get "/" do
+    redcarpet = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    @content = redcarpet.render(File.read(File.dirname(__FILE__) + "/data/2013-08-26.md"))
+    erb :index
+  end
 end
