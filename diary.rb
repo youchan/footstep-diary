1b263c7682cd56f3e38dbe571fbda14f7e0509ae
youchan <youchan01@gmail.com>
2016-01-01 03:54:24 +0900

    Renewal the Year 2016

--- diary.rb
+++ diary.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/assetpack'
+require 'redcarpet'
 
 class App < Sinatra::Base
 
@@ -17,7 +18,7 @@ class App < Sinatra::Base
     ]
 
     css :main, '/css/main.css', [
-      '/css/main.css'
+      '/css/lib/**/*.css'
     ]
 
     js_compression :jsmin
@@ -25,9 +26,24 @@ class App < Sinatra::Base
   end
 
   get "/" do
-    erb :index
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, hard_wrap: true)
+    @contents = Dir.glob("data/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9].md").reverse.map do |filename|
+      { date: filename[/data\/(\d{4}-\d{2}-\d{2})\.md/, 1], entry: markdown.render(File.read(filename))}
+    end
+    haml :index
   end
 
+  get "/:date" do
+    begin
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, hard_wrap: true)
+      entry = markdown.render(File.read("data/#{params[:date]}.md"))
+      @contents = [{ date: params[:date], entry: entry }]
+    rescue
+      @error = "お探しのページは見つかりませんでした…"
+      status 404
+    end
+    haml :index
+  end
 end
 
 if __FILE__ == $0
