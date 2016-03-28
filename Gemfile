716b748b0e6058e7627df1962bc90408007a4d74
youchan <youchan01@gmail.com>
2016-03-29 04:42:33 +0900

    Code syntax highlighting

--- Gemfile
+++ Gemfile
@@ -9,3 +9,4 @@ gem 'builder'
 gem 'sass'
 
 gem 'redcarpet'
+gem 'rouge'
--- diary.rb
+++ diary.rb
@@ -3,6 +3,24 @@ require 'sinatra/asset_pipeline'
 require 'redcarpet'
 require 'builder'
 require 'rss'
+require 'rouge'
+require 'rouge/plugins/redcarpet'
+
+class HTMLwithHighlight < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+  # def block_code(code, language)
+  #   formatter = Rouge::Formatters::HTML.new(css_class: 'highlight', line_numbers: false)
+  #   lexer = case language
+  #           when 'ruby'
+  #             Rouge::Lexers::Ruby.new
+  #           when 'javascript'
+  #             Rouge::Lexers::Javascript.new
+  #           else
+  #             Rouge::Lexers::PlainText.new
+  #           end
+  #   formatter.format(lexer.lex(code))
+  # end
+end
 
 class App < Sinatra::Base
   configure do
@@ -15,7 +33,6 @@ class App < Sinatra::Base
   end
 
   get "/" do
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, hard_wrap: true)
     @contents = Dir.glob("#{settings.root}/data/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9].md").sort.reverse.take(20).map do |filename|
       { date: filename[/(\d{4}-\d{2}-\d{2})\.md/, 1], entry: markdown.render(File.read(filename))}
     end
@@ -23,7 +40,6 @@ class App < Sinatra::Base
   end
 
   get '/rss' do
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, hard_wrap: true)
     @entries = Dir.glob("#{settings.root}/data/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9].md").sort.reverse.take(20).map do |filename|
       title = File.readlines(filename)[0].sub(/\A\#+ +/, '')
       { date: filename[/(\d{4}-\d{2}-\d{2})\.md/, 1], title: title, description: markdown.render(File.read(filename)) }
@@ -35,7 +51,6 @@ class App < Sinatra::Base
   get /\/(\d{4}-\d{2}-\d{2})/ do
     begin
       date = params[:captures].first
-      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, hard_wrap: true)
       md_content = File.read("#{settings.root}/data/#{date}.md")
       entry = markdown.render(md_content)
       @title = md_content.split("\n").first.sub(/^#(.+\Z)/, '\1').strip if md_content =~ /^# /
@@ -46,6 +61,10 @@ class App < Sinatra::Base
       status 404
     end
     haml :index
+  end
+
+  def markdown
+    @markdown ||= Redcarpet::Markdown.new(HTMLwithHighlight, autolink: true, tables: true, hard_wrap: true, fenced_code_blocks: true)
   end
 end
 
